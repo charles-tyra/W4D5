6aea9935c9e8f4a25a014dd268f0a2b3857da5bc
“Emmett <emmett.wechsler11@gmail.com>
2022-12-02 14:49:29 -0500

    anagrams with comments

--- anagrams.rb
+++ anagrams.rb
@@ -4,18 +4,26 @@ def first_anagram?(string, ana)
 end
 
 def permutations(string)
-    return [string] if string.length == 1
+    return [string] if string.length == 1 # this makes sense
 
-    first = string.shift
+    first = string.shift # shifts the first element off of the array
 
-    perms = permutations(string)
+    perms = permutations(string) # passes the array with the first element slided off back into permutations
 
-    total_perms = []
+    # after permutations reaches base case, we are able to continue with the code using first and perms containing separate elements
 
+    total_perms = [] # establish the array you shovel your re-organized elements into
+
+
+    # iterate through perms, and then also through the collection of sliced elements (stored in perms)
+    # the below iteration only happens if string.lenght is 2 or greater, otherwise it is caught by the base case.
     perms.each do |perm|
         (0..perm.length).each do |i|
-            new_str = [perm[0...i]] + [first] + [perm[i..-1]]
-            total_perms << new_str.join("")
+            # new_arr is the combination of the permutated elements with "first" moved into the middle, to 'reorganize' the elements
+            new_arr = [perm[0...i]] + [first] + [perm[i..-1]]
+
+            # easiest part, add a joined version of the new_arr to total perms so we are returning an array of strings by the end.
+            total_perms << new_arr.join("")
         end
     end
 
